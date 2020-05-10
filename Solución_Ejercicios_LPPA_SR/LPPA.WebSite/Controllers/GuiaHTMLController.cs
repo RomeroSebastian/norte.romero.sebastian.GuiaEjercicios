acc8cc5b7ca78cf058a3d6ff7feb1f1095912e8b
Sebastian Romero <sfromero23@gmail.com>
2020-05-10 11:44:44 -0300

    Práctica HTML

---
+++ Solución_Ejercicios_LPPA_SR/LPPA.WebSite/Controllers/GuiaHTMLController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LPPA.WebSite.Controllers
+{
+    public class GuiaHTMLController : Controller
+    {
+        // GET: GuiaHTML
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio1()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio2()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio3()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio4()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio5()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio6()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio7()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio8()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio9()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio10()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio11()
+        {
+            return View();
+        }
+    }
+}
