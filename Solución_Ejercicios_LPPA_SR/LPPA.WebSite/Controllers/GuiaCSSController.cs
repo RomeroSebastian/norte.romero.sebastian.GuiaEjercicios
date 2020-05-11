5441e2f413c359602c30096c3efcbb3b992a21b1
Sebastian Romero <sfromero23@gmail.com>
2020-05-10 22:27:12 -0300

    Arreglos en ejercicios de HTML y Ejercicios 1, 2 y 3 de CSS.

---
+++ Solución_Ejercicios_LPPA_SR/LPPA.WebSite/Controllers/GuiaCSSController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LPPA.WebSite.Controllers
+{
+    public class GuiaCSSController : Controller
+    {
+        // GET: GuiaCSS
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio1()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio2()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio3()
+        {
+            return View();
+        }
+    }
+}
